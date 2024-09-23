a17d7c49cd09a21a93aa6dec4a89f0bbcdb52fa7
afzal-2004 <moa44468@gmail.com>
2024-09-24 02:54:02 +0530

    Adding some Comments

--- Server/Src/Routes/index.js
+++ Server/Src/Routes/index.js
@@ -6,18 +6,17 @@ import {
   Updatecard,
   getUpdatedContact,
 } from '../Controller/upload.Controller.js';
-import { upload } from '../Middleware/Mullter.js';
+import { upload } from '../Middleware/Mullter.js'; //  this is multer middleware for
 const router = Router();
 router.get('/accessdata', accessitems);
-router.post(
-  '/senddata',
-
-  upload.single('File'),
-
-  Senddata
-);
+router.post('/senddata', upload.single('File'), Senddata);
 router.delete('/Deletecard/:id', Deletecard);
 router.put('/updatedata/:id', Updatecard);
 router.get('/getupdateContact/:id', getUpdatedContact);
 
 export default router;
+
+// file or any type of video uploadin in backend is  persfroms in these steps
+// a)first of Fall   through an middleware like multer take file on your local system after taking on local system
+// b)push on cloudniary cloud
+// c)after this url is basically saved inside in your   db
