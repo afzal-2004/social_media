a3f8421e423c098c5c1b44b602241668c3d9460f
afzal-2004 <moa44468@gmail.com>
2024-10-15 23:01:08 +0530

    Debugg

--- Client/src/assets/constant.js
+++ Client/src/assets/constant.js
@@ -1,3 +1,36 @@
-export const BackendUrl = "https://social-media-n4nv.onrender.com";
-// export const BackendUrl = "http://localhost:3000";
+// export const BackendUrl = "https://social-media-n4nv.onrender.com";
+export const BackendUrl = "http://localhost:3000";
 export const token = localStorage.getItem("Token");
+import { jwtDecode } from "jwt-decode";
+
+if (token) {
+  const decoded = jwtDecode(token);
+  console.log("This is My jwt Expiration Time", decoded.exp);
+}
+
+export function isTokenExpired(token) {
+  const decoded = jwtDecode(token);
+  const currentTime = Math.floor(Date.now() / 1000); //
+  return decoded.exp < currentTime;
+}
+
+const Token = localStorage.getItem("Token");
+if (Token && isTokenExpired(Token)) {
+  localStorage.removeItem("token");
+  alert("Session has expired. Please log in again.");
+}
+
+function scheduleTokenRemoval(token) {
+  const decoded = jwtDecode(token);
+  const currentTime = Math.floor(Date.now() / 1000);
+  const expirationTime = (decoded.exp - currentTime) * 1000;
+  setTimeout(() => {
+    localStorage.removeItem("token");
+    alert("Session has expired. Please log in again.");
+  }, expirationTime);
+}
+
+const Localtoken = localStorage.getItem("Token");
+if (Localtoken) {
+  scheduleTokenRemoval(Localtoken);
+}
--- Server/Src/Middleware/Mullter.js
+++ Server/Src/Middleware/Mullter.js
@@ -1,10 +1,11 @@
-import multer from "multer";
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/images");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
+import multer from 'multer';
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './public/images');
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+const storage = multer.memoryStorage();
 export const upload = multer({ storage: storage });
