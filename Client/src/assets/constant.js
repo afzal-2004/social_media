28ac9f4c71ed0e53dfd57579a0d20887a3948c5c
afzal-2004 <moa44468@gmail.com>
2024-10-15 23:45:07 +0530

    debugg

--- Client/src/assets/constant.js
+++ Client/src/assets/constant.js
@@ -1,4 +1,4 @@
-// export const BackendUrl = "https://social-media-n4nv.onrender.com";
+export const BackendUrl = "https://social-media-n4nv.onrender.com";
 // export const BackendUrl = "http://localhost:3000";
 export const token = localStorage.getItem("Token");
 import { jwtDecode } from "jwt-decode";
